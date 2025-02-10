56a87819870dcf5d1ce3c6a2f5f67b414f908013
Bibekdeep Singh <bibekdeepsingh@rrc.ca>
2025-02-10 14:41:38 -0600

    Done with coding challenge.

--- OneDrive/rrc_polytech/courses/Semester 3/COMP-3018 Back-End Development/Coding Challenge/3/src/app.ts
+++ OneDrive/rrc_polytech/courses/Semester 3/COMP-3018 Back-End Development/Coding Challenge/3/src/app.ts
@@ -1,22 +1,11 @@
 import express, { Request, Response, NextFunction, Express } from "express";
 import taskRoutes from "./api/v1/routes/taskRoutes";
+import { errorHandler } from './api/v1/middleware/errorhandler';
 
 const app: Express = express();
 app.use(express.json());
 
 app.use("/api/v1/tasks", taskRoutes);
-
-const express = require("express");
-const app = express();
-const taskRoutes = require("./taskRoutes");
-const errorHandler = require("./errorHandler");
-
-app.use(express.json());
-app.use("/tasks", taskRoutes);
-app.use(errorHandler);
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server runni
 
 /**
  * Global error handler.
@@ -25,5 +14,7 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction): void => {
 	console.error(err);
 	res.status(500).json({ error: "Internal Server Error" });
 });
+
+app.use(errorHandler);
 
 export default app;
